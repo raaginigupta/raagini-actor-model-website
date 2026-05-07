e948ebbdd1999ecbc54fdd798d6a5bab7c846b25
Raagini Gupta <raaginigupta@Raaginis-Mac-mini.local>
2026-05-07 00:56:29 -0400

    Create actor and model website

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector(".site-header");
+
+const setHeaderState = () => {
+  header.classList.toggle("is-scrolled", window.scrollY > 12);
+};
+
+setHeaderState();
+window.addEventListener("scroll", setHeaderState, { passive: true });
